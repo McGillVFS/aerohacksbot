39ca6c9d17abd21ae3513f4dca3811dc7b04b79a
ScaredBow <31490393+ScaredBow@users.noreply.github.com>
2026-02-06 21:51:25 -0500

    Add role assignment after verify

--- pages/api/discord/interactions.ts
+++ pages/api/discord/interactions.ts
@@ -376,7 +376,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   res.status(200).json(interactionDeferredResponse());
-  setImmediate(() => {
-    void processVerifyInteraction(interaction);
-  });
+  await processVerifyInteraction(interaction);
 }
